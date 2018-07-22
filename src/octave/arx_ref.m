f646c33c05a088192f2958373566f4f458757c47
Martin Fägerlind <martin@localhost.localdomain>
2018-07-22 21:49:20 +0200

    Added octave reference code for ARX idenification.

---
+++ src/octave/arx_ref.m
@@ -0,0 +1,25 @@
+pkg load control;
+
+f = fopen("../sim.txt");
+C = textscan(f, "%s %s %s %s", 1);
+simDat = textscan(f, "%f %f %f %f", "delimiter", " ");
+fclose(f);
+
+t = simDat{1};
+u = simDat{2};
+y = simDat{3};
+y_sim = simDat{4};
+
+dat = iddata(y, u);
+
+na = 4;
+nb = 4;
+tf_sys = arx(dat, "na", na, "nb", nb);
+[sys, x0] = arx(dat, "na", na, "nb", nb);
+
+y_ref = lsim(sys, u, t, x0);
+
+figure();
+plot(t, y, "b");
+hold on;
+plot(t, y_ref, "r--");
