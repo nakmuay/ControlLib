ffcc2446eaed2f71f5aacdc0174b020be2e6a3d5
Martin <martin@localhost.localdomain>
2018-11-22 23:08:34 +0100

    Continue implementing ARX method.

--- src/python/models.py
+++ src/python/models.py
@@ -1,15 +1,25 @@
-from robustness import assert_nonnegative
+from iddata import iddata
+from robustness import assert_nonnegative, \
+                       assert_type
 
 def arx(dat, na, nb):
 
     # Validate inputs
     #TODO: check input data is supported
+    assert_type(dat, iddata)
     assert_nonnegative(na)   
     assert_nonnegative(nb)   
  
     # Extract data
-    #y = dat.y
-    #u = dat.u
+    # TODO: Assume only one experiment for now
+    (exp_samp, _, _, _) = dat.shape
+    n_samp = exp_samp[0]
+
+    y = dat.y[0]
+    u = dat.u[0]
 
     # Check maximum delay
     max_n = max(na, nb)
+
+    print(y)
+
--- src/python/test_arx.py
+++ src/python/test_arx.py
@@ -1,3 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from iddata import iddata
 from models import arx
 
-arx([], 1, 1)
+num_samp = 100
+f1 = 0.01
+f2 = 0.04
+t = np.array(range(num_samp))
+u = np.sin(2*np.pi*f1*t)
+y = u + 1/2*np.sin(2*np.pi*f2*t)
+
+dat = iddata(y, u)
+
+#dat.plot()
+
+arx(dat, 1, 1)
