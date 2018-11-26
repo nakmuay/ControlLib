d2f7dc09f09596157b94ee0e083ae9c1a4be7255
Martin <martin@localhost.localdomain>
2018-11-26 21:09:14 +0100

    Add noise to test data.

--- src/python/test_arx.py
+++ src/python/test_arx.py
@@ -7,12 +7,12 @@ from models import arx
 num_samp = 10
 f1 = 0.01
 f2 = 0.04
-#t = np.array(range(num_samp))
-#u = np.sin(2*np.pi*f1*t)
-#y = u + 1/2*np.sin(2*np.pi*f2*t)
+t = np.array(range(num_samp))
+u = np.sin(2*np.pi*f1*t)
+y = u + 1/2*np.sin(2*np.pi*f2*t) + 1/10*np.random.rand(num_samp)
 
-u = np.array(range(10)) + 1
-y = u + 10
+#u = np.array(range(10)) + 1
+#y = u + 10
 
 dat = iddata(y, u)
 
