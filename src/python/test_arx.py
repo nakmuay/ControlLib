e8e5b64734347c4ed99bcedbae1a619d6042d109
Martin <mfagerlind@hotmail.com>
2018-11-28 21:37:10 +0100

    Cleanup.

--- src/python/test_arx.py
+++ src/python/test_arx.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 from iddata import iddata
 from models import arx
 
-num_samp = 200
+num_samp = 300
 f1 = 0.01
 f2 = 0.04
 t = np.array(range(num_samp))
 u = np.sin(2*np.pi*f1*t)
-y = u + 1/2*np.sin(2*np.pi*f2*t) + 1/10*np.random.rand(num_samp)
+y = u + 1/2*np.sin(2*np.pi*f2*t) + 1/100*np.random.rand(num_samp)
 
 #u = np.array(range(10)) + 1
 #y = u + 10
@@ -18,8 +18,8 @@ y = u + 1/2*np.sin(2*np.pi*f2*t) + 1/10*np.random.rand(num_samp)
 dat = iddata(y, u)
 m = arx(dat, 10, 10)
 
-_, y_hat = signal.dlsim(m, u)
+t_out, y_out = signal.dlsim(m, u)
 
 plt.plot(t, y)
-plt.plot(t, y_hat, 'r--')
+plt.plot(t_out, y_out, 'r--')
 plt.show()
