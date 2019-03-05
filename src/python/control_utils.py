3cd27a6a9293605a58687f08034092811e039ba9
Martin <martin@localhost.localdomain>
2019-03-05 21:32:23 +0100

    Add missing file.

---
+++ src/python/control_utils.py
@@ -0,0 +1,36 @@
+import numpy as np
+import scipy.linalg as linalg
+
+def normalize(arr):
+    return (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
+
+def find_init_states(sys, y, u, horizon=None):
+    A = sys.A
+    B = sys.B
+    C = sys.C
+    D = sys.D
+
+    if not horizon:
+        horizon = len(y)
+
+    _, n_states = np.shape(C)
+
+    lhs = np.zeros((horizon, 1))
+    rhs = np.zeros((horizon, n_states))
+
+    lhs[0] = y[0]
+    rhs[0, :] = C
+    for i in (n+1 for n in range(horizon-1)):
+        CAprod = C
+        CABSum = 0.0
+
+        for j in reversed(range(i)):
+            CABSum = CABSum + np.dot(CAprod, B*u[j])
+            CAprod = np.dot(CAprod, A)
+
+        lhs[i] = y[i] - CABSum - D*u[i]
+        rhs[i, :] = CAprod
+
+    x0, _, _, _ = linalg.lstsq(rhs, lhs, cond=None)
+    # TODO: Is it ok to flatten the result like this?
+    return x0.reshape(1, x0.size)
